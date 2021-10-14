bc428c1e90fb7c566192ff1e51e2b91f28cbd20c
by-student-2017 <studentsctest@gmail.com>
2021-10-14 11:03:17 +0900

    made on cygwin

--- DFTBplus20.2.1Windows10-CygWin_Netlib/dftb+/lib/cmake/dftbplus/dftbplus-targets-relwithdebinfo.cmake
+++ DFTBplus20.2.1Windows10-CygWin_Netlib/dftb+/lib/cmake/dftbplus/dftbplus-targets-relwithdebinfo.cmake
@@ -5,6 +5,16 @@
 # Commands may need to know the format version.
 set(CMAKE_IMPORT_FILE_VERSION 1)
 
+# Import target "DftbPlus::dftd3" for configuration "RelWithDebInfo"
+set_property(TARGET DftbPlus::dftd3 APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
+set_target_properties(DftbPlus::dftd3 PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELWITHDEBINFO "Fortran"
+  IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libdftd3.a"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS DftbPlus::dftd3 )
+list(APPEND _IMPORT_CHECK_FILES_FOR_DftbPlus::dftd3 "${_IMPORT_PREFIX}/lib/libdftd3.a" )
+
 # Import target "DftbPlus::mudpack" for configuration "RelWithDebInfo"
 set_property(TARGET DftbPlus::mudpack APPEND PROPERTY IMPORTED_CONFIGURATIONS RELWITHDEBINFO)
 set_target_properties(DftbPlus::mudpack PROPERTIES
